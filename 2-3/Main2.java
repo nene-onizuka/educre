c0c624c6e196d90c4fc5b27b5f49f3907151bbc7
nene-onizuka <16onizuka2@gmail.com>
2025-05-07 09:12:25 +0900

    Add files via upload

---
+++ 2-3/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+    public static void main(String[] args) {
+      System.out.println("今日の天気: 晴れ");     //今日の天気
+      System.out.println("気温: 23.5℃");    //気温
+
+    }
+}
---
+++ 2-3/Main3.java
@@ -0,0 +1,13 @@
+/**
+ *class名はMain3
+ *Hello,Worldto出力させる
+ */
+
+/*Hello,Worldと
+出力させる*/
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");   //Hello,World
+    }
+}
