86aa58d75fe0450a58165d8ffbe29256cfb41fa5
nene-onizuka <16onizuka2@gmail.com>
2025-05-10 00:26:54 +0900

    Add files via upload

--- 2-7/Main6.java
+++ 2-7/Main6.java
@@ -3,16 +3,23 @@
  * ボーナス経験値として25％増加(小数点以下切り捨て)
  * 増加後の経験値を表示
  */
+import java.util.Scanner;
+
 public class Main6 {
     public static void main(String[] args) {
-        int experiencePoints;
-        double bonusExperiencePoints;
+        Scanner scanner = new Scanner (System.in);
 
+        System.out.print("現在の経験値を入力してください：" );
+        String experiencePointsStr = scanner.nextLine();
+        int experiencePoints =Integer.parseInt(experiencePointsStr);
         experiencePoints =1000;
+
+        System.out.print("ボーナス後の経験値：" );
+        String bonusExperiencePointsStr = scanner.nextLine();
+        double bonusExperiencePoints = Integer.parseInt(bonusExperiencePointsStr);
         bonusExperiencePoints =experiencePoints *1.25;
 
-        System.out.println("現在の経験値を入力してください：" +experiencePoints);
-        System.out.println("ボーナス後の経験値：" +(int)bonusExperiencePoints);
+        scanner.close();
     
     }
     
--- 2-7/Main8.java
+++ 2-7/Main8.java
@@ -4,22 +4,34 @@
  * 両方のダメージを表示
  */
 
+import java.util.Scanner;
+
 public class Main8 {
     public static void main(String[] args){
-        int atackPower;
-        int defensePower;
-        int damage;
-        double criticalDamage;
+        Scanner scanner = new Scanner (System.in);
 
-        atackPower =50;
-        defensePower =20;
-        damage =atackPower -defensePower;
-        criticalDamage =damage *1.5;
+        System.out.print("攻撃力を入力してください：");
+        String atackPowerStr = scanner.nextLine();
+        int atackPower = Integer.parseInt(atackPowerStr);
+        atackPower = 50;
 
-        System.out.println("攻撃力を入力してください：" +atackPower);
-        System.out.println("防御力を入力してください：" +defensePower);
-        System.out.println("通常ダメージ：" +damage);
-        System.out.println("クリティカルダメージ："+(int)criticalDamage);
+        System.out.print("防御力を入力してください：" );
+        String defensePowerStr = scanner.nextLine();
+        int defensePower = Integer.parseInt(defensePowerStr);
+        defensePower = 20;
+
+        System.out.print("通常ダメージ：");
+        String damageStr = scanner.nextLine();
+        int damage = Integer.parseInt(damageStr);
+        damage =atackPower - defensePower;
+        
+        System.out.print("クリティカルダメージ：");
+        String criticalDamageStr = scanner.nextLine();
+        double criticalDamage = Integer.parseInt(criticalDamageStr);
+        criticalDamage = damage *1.5;
+
+        
+        scanner.close();
 
     } 
     
